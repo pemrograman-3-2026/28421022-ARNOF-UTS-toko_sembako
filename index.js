a0cbacb6e72cb6c9ab990b90f28b56bb17462704
arnofinformatika <arnofinformatika@gmail.com>
2026-06-04 15:46:40 +0700

    import cors

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ import UserRoute from './routes/user.route.js'
 import KategoriRoute from './routes/kategori.route.js'
 import BarangRoute from './routes/barang.route.js'
 import TransaksiRoute from './routes/transaksi.route.js'
+import path from 'path'
+import cors from 'cors'
 
 const app = express()
 app.use(express.json())
+app.use(cors())
+
+const imagePath = express.static(path.join(process.cwd(), 'uploads'))
+app.use('/image', imagePath)
+
 
 app.get('/', (req, res) => {
     res.send("Toko Sembako is running!")
@@ -16,6 +23,6 @@ app.use('/kategori', KategoriRoute)
 app.use('/barang', BarangRoute)
 app.use('/transaksi', TransaksiRoute)
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000')
+app.listen(3100, () => {
+    console.log('Server started')
 })
